97614c245e719c658258a74beaaf55b244009503
linearqueue <24204031154@stu.manit.ac.in>
2026-06-20 19:36:46 +0530

    Part_2_Completed

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,17 +1,50 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
-  const position = JSON.parse(note.position);
+  const [position, setPosition] = useState(JSON.parse(note.position));
+  const texAreaRef = useRef(null);
+  const cardRef = useRef(null);
+  const mouseStartPos = useRef({ x: 0, y: 0 });
+
   const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
 
-  const texAreaRef = useRef(null);
-
   function autoGrow(texAreaRef) {
     const { current } = texAreaRef;
+
     current.style.height = "auto";
     current.style.height = current.scrollHeight + "px";
+  }
+
+  function mouseMove(e) {
+    let mouseMoveDir = {
+      x: mouseStartPos.current.x - e.clientX,
+      y: mouseStartPos.current.y - e.clientY,
+    };
+
+    mouseStartPos.current.x = e.clientX;
+    mouseStartPos.current.y = e.clientY;
+
+    setPosition({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  }
+
+  function mouseUp() {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  }
+
+  function mouseDown(e) {
+    mouseStartPos.current.x = e.clientX;
+    mouseStartPos.current.y = e.clientY;
+
+    console.log(mouseStartPos.current);
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
   }
 
   useEffect(() => {
@@ -26,10 +59,12 @@ const NoteCard = ({ note }) => {
         left: `${position.x}px`,
         top: `${position.y}px`,
       }}
+      ref={cardRef}
     >
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown={mouseDown}
       >
         <Trash />
       </div>
